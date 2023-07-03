6e4ace4600eef72becbd61c44406927be2b5653a
Lokeswaran Shrishiva <115989662+lokietechie@users.noreply.github.com>
2023-07-03 21:13:32 +1200

    Add files via upload

---
+++ Bubblesort.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+
+using namespace std;
+
+void swap();
+int main()
+{
+    int a[100],n,temp;
+    cout<< "Enter the no of elements for sorting";
+    cin>> n;
+    for(int i=1; i<=n;i++)
+    {
+        cout<<"Enter the "<< i << " element=  ";
+        cin>> a[i];
+    }
+
+    for(int i=1; i<=n;i++)
+    {
+    for(int j=1; j<n;j++)
+    {
+        if(a[j]>a[j+1])
+        {
+            temp = a[j];
+            a[j] = a[j+1];
+            a[j+1] = temp;
+        }
+    }
+    }
+     for(int i=1; i<=n;i++)
+    {
+        cout<<a[i]<<'\n';
+    }
+}
+
+void swap()
+{
+
+}
